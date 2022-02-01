cfc0196c43c2a24641cd2d93224224091dc174d1
erfan mahdavi athar <erfan.mh.at@gmail.com>
2022-02-01 12:01:32 +0330

    update AlsatIPGAndorid to V1.4.2

--- app/src/main/java/com/alsatpardakht/alsatipgandroidjavaexample/MainActivity.java
+++ app/src/main/java/com/alsatpardakht/alsatipgandroidjavaexample/MainActivity.java
@@ -1,7 +1,7 @@
 package com.alsatpardakht.alsatipgandroidjavaexample;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -11,17 +11,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alsatpardakht.alsatipgandroid.AlsatIPG;
-import com.alsatpardakht.alsatipgandroid.core.callback.PaymentSignCallback;
-import com.alsatpardakht.alsatipgandroid.core.callback.PaymentValidationCallback;
+import com.alsatpardakht.alsatipgandroid.data.remote.model.PaymentSignRequest;
 import com.alsatpardakht.alsatipgandroid.domain.model.PaymentSignResult;
 import com.alsatpardakht.alsatipgandroid.domain.model.PaymentValidationResult;
-import com.alsatpardakht.ipg.data.remote.model.PaymentSignRequest;
 
-public class MainActivity extends AppCompatActivity implements PaymentSignCallback, PaymentValidationCallback {
+public class MainActivity extends AppCompatActivity {
 
     private final String API = "ENTER YOUR API KEY HERE";
 
-    private final AlsatIPG alsatIPG = AlsatIPG.getInstance(this, this);
+    private final AlsatIPG alsatIPG = AlsatIPG.getInstance();
 
     private TextView logTextView;
     private Button signPaymentButton;
@@ -40,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements PaymentSignCallba
                 signPaymentButtonOnClick();
             }
         });
+
+        observeToPaymentSignStatus();
+        observeToPaymentValidationStatus();
     }
 
     @Override
@@ -58,37 +59,45 @@ public class MainActivity extends AppCompatActivity implements PaymentSignCallba
         }
     }
 
-    @Override
-    public void onPaymentSignResult(@NonNull PaymentSignResult paymentSignResult) {
-        if (paymentSignResult.isSuccessful()) {
-            log("payment Sign Success url = " + paymentSignResult.getUrl());
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(paymentSignResult.getUrl()));
-            startActivity(intent);
-        } else if (paymentSignResult.isLoading()) {
-            log("payment Sign Loading ...");
-        } else {
-            log("payment Sign error = " + paymentSignResult.getErrorMessage());
-        }
+    public void observeToPaymentSignStatus() {
+        alsatIPG.getPaymentSignStatus().observe(this, new Observer<PaymentSignResult>() {
+            @Override
+            public void onChanged(PaymentSignResult paymentSignResult) {
+                if (paymentSignResult.isSuccessful()) {
+                    log("payment Sign Success url = " + paymentSignResult.getUrl());
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(paymentSignResult.getUrl()));
+                    startActivity(intent);
+                } else if (paymentSignResult.isLoading()) {
+                    log("payment Sign Loading ...");
+                } else if (paymentSignResult.getError() != null) {
+                    log("payment Sign error = " + paymentSignResult.getError().getMessage());
+                }
+            }
+        });
     }
 
-    @Override
-    public void onPaymentValidationResult(@NonNull PaymentValidationResult paymentValidationResult) {
-        if (paymentValidationResult.isSuccessful()) {
-            log("payment Validation Success data = " + paymentValidationResult.getData());
-            if (
-                    (paymentValidationResult.getData() != null) &&
-                            (paymentValidationResult.getData().getPSP().getIsSuccess()) &&
-                            (paymentValidationResult.getData().getVERIFY().getIsSuccess())
-            ) {
-                log("money transferred");
-            } else {
-                log("money has not been transferred");
+    public void observeToPaymentValidationStatus() {
+        alsatIPG.getPaymentValidationStatus().observe(this, new Observer<PaymentValidationResult>() {
+            @Override
+            public void onChanged(PaymentValidationResult paymentValidationResult) {
+                if (paymentValidationResult.isSuccessful()) {
+                    log("payment Validation Success data = " + paymentValidationResult.getData());
+                    if (
+                            (paymentValidationResult.getData() != null) &&
+                                    (paymentValidationResult.getData().getPSP().getIsSuccess()) &&
+                                    (paymentValidationResult.getData().getVERIFY().getIsSuccess())
+                    ) {
+                        log("money transferred");
+                    } else {
+                        log("money has not been transferred");
+                    }
+                } else if (paymentValidationResult.isLoading()) {
+                    log("payment Validation Loading ...");
+                } else if (paymentValidationResult.getError() != null) {
+                    log("payment Validation error = " + paymentValidationResult.getError().getMessage());
+                }
             }
-        } else if (paymentValidationResult.isLoading()) {
-            log("payment Validation Loading ...");
-        } else {
-            log("payment Validation error = " + paymentValidationResult.getErrorMessage());
-        }
+        });
     }
 
     private void signPaymentButtonOnClick() {
